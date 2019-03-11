dfb1277a4f9044dff1cc2cbecefd68fcc03f697e
Vanessa Prema <vanessabprema@gmail.com>
2019-03-11 16:45:41 -0400

    fixed password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
    has_many :favorites
    has_many :movies, through: :favorites
+   has_secure_password
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(username: 'vprema', password_digest: 'max')
+User.create(username: 'vprema', password: 'max')
